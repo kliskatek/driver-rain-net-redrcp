6cc6c8edfb7f7f044528a55dfa86af46827b810f
aritz-alonso <58985478+aritz-alonso@users.noreply.github.com>
2024-09-26 11:34:42 +0200

    Initial commit

---
+++ csharp/REDRCP/REDRCP/REDRCP.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Ports;
+using Serilog;
+
+namespace Kliskatek.REDRCP
+{
+    /// <summary>
+    /// Provides higher level access to common RED RCP functionality. Works with serial port connection.
+    /// </summary>
+    public partial class REDRCP
+    {
+        private SerialPort _serialPort;
+
+        public bool Connect(string serialPort)
+        {
+            try
+            {
+                _serialPort = new SerialPort();
+                _serialPort.PortName = serialPort;
+                _serialPort.BaudRate = 115200;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Warning(e, "Exception thrown : ");
+                return false;
+            }
+        }
+    }
+}
---
+++ csharp/REDRCP/REDRCPConsoleDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Kliskatek.REDRCP;
+
+Console.WriteLine("Hello, World!");
+
+var reader = new REDRCP();
